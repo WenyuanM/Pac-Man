80c3f0579c2ee94e4687806205320eb7fe0c9c77
WenyuanM <nancymapic@gmail.com>
2017-05-29 17:04:40 -0700

    Add files via upload

--- Ghost.java
+++ Ghost.java
@@ -1,25 +1,53 @@
 import java.awt.*;
 import java.awt.image.BufferedImage;
 
-/**
- * Created by Nancy on 2017/5/28.
- */
 public class Ghost extends Character{
+    private int _count;
     public Ghost(String... imageNames){
+        _count = 0;
         initialImages(imageNames);
         initialCharacter(new Coordinate(0,0),new Coordinate(0,0),0,0,'N');
     }
     public Ghost(Coordinate pos,Coordinate speed,int row,int col,char name,String...imageNames){
+        _count = 0;
         initialImages(imageNames);
         initialCharacter(pos,speed,row,col,name);
     }
 
-    public BufferedImage get_images(){
+    public BufferedImage get_images(boolean gameMode){
+        int remainder = _count % 2;
+        _count++;
+        if(_die){
+            if(remainder == 0){
+                return _images[7];
+            }
+            else{
+                return _images[8];
+            }
+        }
+        if(!gameMode){
+            if(remainder == 0){
+                return _images[5];
+            }
+            else{
+                return _images[6];
+            }
+        }
         if(_movingDirection == '-' || _movingDirection == 'D' || _movingDirection == 'U'){
-            return _images[0];
+            if(remainder == 0){
+                return _images[0];
+            }
+            else{
+                return _images[1];
+            }
         }
         if(_movingDirection == 'L'){
-            return _images[2];
+            if(remainder == 0){
+                return _images[2];
+            }
+            else{
+                return _images[3];
+            }
         }
         if(_movingDirection == 'R'){
             return _images[4];
@@ -27,8 +55,8 @@ public class Ghost extends Character{
         return null;
     }
 
-    public void draw(Graphics shape){
-        BufferedImage image = get_images();
+    public void draw(Graphics shape,boolean gameMode){
+        BufferedImage image = get_images(gameMode);
         int size = Constants.CHARACTER_IMAGE_SIZE;
         shape.drawImage(image,(int)(_position.getX()-size/2),(int)(_position.getY()-size/2),size,size,null);
     }
